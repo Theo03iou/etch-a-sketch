282cceb437ecba64133b3e709ee04167ab08628f
theo03iou <theo03iou@outlook.com>
2023-01-11 22:52:59 +0000

    Add style.css

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
-console.log("Fesfsaf");
+const divContainer = document.querySelector('#container'); // Targets the container
+const div = document.createElement('div'); // Creates an individual div.
 
-const divContainer = document.querySelector('#container');
+div.setAttribute("class", "gridDiv") // Sets the class of gridDiv to the JS generated div.
+let gridSize = prompt("enter the size of the array")
 
-
-const div = document.createElement('div');
-div.setAttribute("class", "gridDiv")
-
-
-
-
-for (let i = 0; i <= 15; i++) {
+for (let i = 0; i <= (gridSize * 2); i++) { // Creates 16 divs
     divContainer.appendChild(div.cloneNode(true));
 }
 
+const gridDiv = document.querySelectorAll(".gridDiv");
+
+
+
+// gridDiv.addEventListener('click', function (e) {
+//     e.target.style.background = 'blue';
+//   }); 
+
+  gridDiv.forEach((div) => {
+    div.addEventListener('mouseover', (e) => {
+      e.target.style.background = 'blue';
+    });
+  });
+
+
+
+
+
+
+
+// gridDiv.addEventListener("mouseover", addColour);
+
+// divContainer.addEventListener("mouseover", addColour);
+
+// function addColour(e) {
+//     e.preventDefault;
+//     console.log("yguyguig");
+//     // gridDiv[e].classList.add("coloured")
+//     gridDiv.style.color = 'blue'; 
+    
+// }
 
